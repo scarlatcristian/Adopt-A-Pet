44465f858685fbfd047b38853dcf322373f21c8f
cristi <scarlat.cristian.gabriel@gmail.com>
2023-04-12 19:40:18 +0300

    added Sass architecture

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const leftIcon = document.querySelector(".header__logo--left");
 const rightIcon = document.querySelector(".header__logo--right");
 
 const addIcons = () => {
-  setTimeout(() => (rightIcon.style.opacity = 0.7), 600);
-  setTimeout(() => (leftIcon.style.opacity = 0.7), 1200);
+  setTimeout(() => (rightIcon.style.opacity = 0.8), 600);
+  setTimeout(() => (leftIcon.style.opacity = 0.8), 1200);
 };
 addIcons();
