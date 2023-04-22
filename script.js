7de0184a7cba608757e1a5f7725d158dcf1f72ed
cristi <scarlat.cristian.gabriel@gmail.com>
2023-04-22 23:50:00 +0300

    added more media queries and fixing bugs

--- script.js
+++ script.js
@@ -2,25 +2,9 @@
 
 const leftIcon = document.querySelector(".header__logo--left");
 const rightIcon = document.querySelector(".header__logo--right");
-const popup = document.getElementById("popup");
-const popupBtn = document.getElementById("popup__btn");
 
 const addIcons = () => {
   setTimeout(() => (rightIcon.style.opacity = 0.8), 600);
   setTimeout(() => (leftIcon.style.opacity = 0.8), 1200);
 };
 addIcons();
-
-popup.addEventListener("click", (e) => {
-  if (e.target === popup) {
-    popup.style.opacity = 0;
-    popup.style.visibility = "hidden";
-    popup.style.transform = "transform: translate(-50%, -50%) scale(0.1)";
-  }
-});
-
-popupBtn.addEventListener("click", () => {
-  popup.style.opacity = 1;
-  popup.style.visibility = "visible";
-  popup.style.transform = "transform: translate(-50%, -50%) scale(1)";
-});
